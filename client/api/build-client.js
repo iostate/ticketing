65c8efba9dbd883e34d6753aa3ce37d5d7a527d2
Quan Truong <qmtruong92@gmail.com>
2023-07-25 12:01:26 -0700

    add note about axios

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,33 +1,18 @@
 // import axios from 'axios';
 
-// // destructure req from context
-// export default ({ req }) => {
-//   if (typeof window === 'undefined') {
-//     console.log('running on server');
-//     // running on server
-//     return axios.create({
-//       baseUrl: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-//       headers: req.headers,
-//     });
-//   } else {
-//     console.log('running on client');
-//     return axios.create({
-//       baseUrl: '/',
-//     });
-//   }
-// };
-import axios from 'axios';
-
+// destructure req from context
 export default ({ req }) => {
   if (typeof window === 'undefined') {
-    // We are on the server
+    console.log('running on server');
+    // running on server
 
     return axios.create({
+      // baseURL allows axios global config
       baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers,
     });
   } else {
-    // We must be on the browser
+    console.log('running on client');
     return axios.create({
       baseUrl: '/',
     });
