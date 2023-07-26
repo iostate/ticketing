52f732fc1ea2b8573b4058a68e8e8d21ece8ad42
Quan Truong <qmtruong92@gmail.com>
2023-07-26 15:49:14 -0700

    Add ticketing service and db k8 depl

---
+++ tickets/src/app.ts
@@ -0,0 +1,54 @@
+// this file is for running this service locally
+// grabs the secrets from disk
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+// ASYNC FILE READ
+// async function getKeyAndCertificate() {
+//   try {
+//     const key = await fs.readFile('../../localhost-key.pem');
+//     const certificate = await fs.readFile('../../localhost.pem');
+//     return { key, certificate };
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// getKeyAndCertificate().then((data) => console.log(data));
+
+// HTTPS CERTIFICATES AND KEYS
+// const httpsKey = fs.readFileSync('../../key.pem', 'utf-8');
+// const httpsCertificate = fs.readFileSync('../../server.crt', 'utf-8');
+// const keyAndCertificate = { key: httpsKey, cert: httpsCertificate };
+
+const app = express();
+
+app.use(json());
+app.set('trust proxy', true);
+
+app.use(
+  cookieSession({
+    signed: false,
+    keys: ['key1', 'key2'],
+    // credentials: true
+    secure: process.env.NODE_ENV !== 'test', // jest automatically sets this to test
+  })
+);
+// ROUTES
+app.use(createTicketRouter);
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// CATCH ALL ROUTE
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+// ERROR HANDLING MIDDLEWARE
+app.use(errorHandler);
+
+export { app };
---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+// const server = https.createServer(keyAndCertificate, app);
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/tickets');
+
+    // await mongoose.connect(
+    //   'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.0'
+    // );
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000.........');
+});
+
+start();
