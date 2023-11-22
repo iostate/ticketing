22a634277eed05f7253503debabc494e85d327b4
Quan Truong <qmtruong92@gmail.com>
2023-11-22 15:22:53 -0700

    remove debug line

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,8 +3,6 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
 import cookieSession from 'cookie-session';
-
-console.log('Delete this later... Experiencing a build issue in kubernetes');
 // Routes
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
